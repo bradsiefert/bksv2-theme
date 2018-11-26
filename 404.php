bf15384c0285ec159c87dc74343c680493951b94
Brad <bradsiefert@icloud.com>
2018-11-26 17:02:13 -0600

    404 page is done

--- 404.php
+++ 404.php
@@ -3,11 +3,9 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
-        <h1>404 Error.</h1>
-        <section class="entry-content">
-          <img src="<?= get_theme_file_uri("images/clueless-my-bad.gif") ?>" alt="">
-          <p><?php _e( 'Nothing found for the requested page. Try a search instead?' ); ?></p>
-        </section>
+        <h1>404 Error</h1>
+        <img class="img-responsive mb-16" src="<?= get_theme_file_uri("images/clueless-my-bad.gif") ?>" alt="Clueless My Bad Gif">
+        <p>Nothing found for the requested page. Click here to go back to the <a href="/">home page</a>.</p>
       </div>
     </div>
   </div>
