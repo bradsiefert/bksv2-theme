3c65eae3afc4cef252b3e2f18074f9b36a53fd96
Brad <bradsiefert@icloud.com>
2018-11-27 17:59:10 -0600

    Adding favicon and cleaning up header partials

--- front-page.php
+++ front-page.php
@@ -1,40 +1,8 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-  <title>Brad Siefert | Product Designer</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="<?= get_theme_file_uri("css/app.css") ?>" />
-</head>
+<?php get_template_part( 'partials/head'); ?>
 <body>
 
 <div class="landing-hero">
-  <nav class="navbar navbar-expand-md navbar-dark">
-    <div class="container">
-      <a href="/" class="navbar-brand">
-        <img src="<?= get_theme_file_uri("images/logo.svg") ?>" alt="Logo for Brad Siefert">
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarToggle"
-        aria-controls="navbarToggle"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarToggle">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item"><a class="nav-link" href="/portfolio/">Portfolio</a></li>
-          <li class="nav-item"><a class="nav-link" href="/about/">About</a></li>
-          <li class="nav-item"><a class="nav-link" href="/blog/">Blog</a></li>
-          <li class="nav-item"><a class="nav-link" href="/contact/">Contact</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <?php get_template_part( 'partials/navbar'); ?>
 
   <div class="container">
     <div class="row">
---
+++ partials/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <title>Brad Siefert | Product Designer</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon" sizes="180x180" href="<?= get_theme_file_uri("images/apple-touch-icon.png") ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?= get_theme_file_uri("images/favicon-32x32.png") ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?= get_theme_file_uri("images/favicon-16x16.png") ?>">
+  <link rel="manifest" href="<?= get_theme_file_uri("images/site.webmanifest") ?>">
+  <link rel="mask-icon" href="<?= get_theme_file_uri("images/safari-pinned-tab.svg") ?>" color="#121212">
+  <link rel="shortcut icon" href="<?= get_theme_file_uri("images/favicon.ico") ?>">
+  <meta name="msapplication-TileColor" content="#121212">
+  <meta name="msapplication-config" content="<?= get_theme_file_uri("images/browserconfig.xml") ?>">
+  <meta name="theme-color" content="#ffffff">
+  <link rel="stylesheet" type="text/css" href="<?= get_theme_file_uri("css/app.css") ?>" />
+</head>
