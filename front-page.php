569d43654168c339a436cd0c171687eeec4db208
Brad <bradsiefert@icloud.com>
2018-12-11 15:28:48 -0600

    Removing “semi-pro” from my life

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@
       <div class="col-lg-12 mt-96 mb-160">
         <h1 class="display-1">
           Hi, I'm Brad, I’m a UX/UI <span class="landing-badge">designer <div class="badge badge-primary">My Day Job
-          </div></span> and semi-pro <span class="landing-badge">photographer <div class="badge badge-success">
+          </div></span> and <span class="landing-badge">photographer <div class="badge badge-success">
             My Passion</div></span> based in Chicago, IL.
         </h1>
         <a href="https://www.brad.si/efert/portfolio/" class="btn btn-outline btn-primary btn-lg mb-16">Design Work</a>
