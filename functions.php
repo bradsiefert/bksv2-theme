20d7c93e8a4079484239d6d1eb03fc1fd2e3ae38
Brad Siefert <bradsiefert@icloud.com>
2020-04-11 14:54:28 -0500

    Clean up js and formatting

--- functions.php
+++ functions.php
@@ -1,18 +1,17 @@
 <?php
 
+  // Adding post thumbnail
+  add_theme_support( 'post-thumbnails' );
 
-// Adding post thumbnail
-add_theme_support( 'post-thumbnails' );
+  // Shortening the excerpt to a # of words
+  add_filter( 'excerpt_length', function($length) {
+      return 48;
+  } );
 
-// Shortening the excerpt to a # of words
-add_filter( 'excerpt_length', function($length) {
-    return 48;
-} );
-
-// Removing the [...] ellipses from blog posts
-function new_excerpt_more() {
-    return '&#46;&#46;&#46;';
-}
-add_filter('excerpt_more', 'new_excerpt_more');
+  // Removing the [...] ellipses from blog posts
+  function new_excerpt_more() {
+      return '&#46;&#46;&#46;';
+  }
+  add_filter('excerpt_more', 'new_excerpt_more');
 
 ?>
