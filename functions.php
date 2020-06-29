f08fbdd50d2f4747dac7667a2bd0495afd442643
Brad Siefert <bradsiefert@icloud.com>
2020-06-29 14:56:05 -0500

    Adding Gutenberg

--- functions.php
+++ functions.php
@@ -13,5 +13,11 @@
       return '&#46;&#46;&#46;';
   }
   add_filter('excerpt_more', 'new_excerpt_more');
-
+  
+  /* Widen the Gutenberg Editor */
+  add_theme_support('editor-styles');
+  function custom_gutenberg_styles() {
+  wp_enqueue_style('custom-gutenberg', get_theme_file_uri( '/css/gutenberg.css' ), false, '1.0', 'all');
+  }
+  add_action('enqueue_block_editor_assets', 'custom_gutenberg_styles');
 ?>
